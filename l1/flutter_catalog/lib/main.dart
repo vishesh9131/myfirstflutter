ef9c64b5b871c7b7c4bb0e50156dec5f020d3e2c
vishesh9131 <sciencely98@gmail.com>
2022-06-29 02:54:18 -0800

    day 4 done

--- l1/flutter_catalog/lib/main.dart
+++ l1/flutter_catalog/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/HomePage.dart';
 import 'package:flutter_catalog/pages/loginPage.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(myapp());
@@ -12,16 +14,18 @@ class myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
+      
       // home:HomePage(),
       themeMode: ThemeMode.dark,
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(
+      primaryTextTheme:GoogleFonts.latoTextTheme(),
+      primarySwatch: Colors.red),
       darkTheme: ThemeData(
         brightness: Brightness.light,
       ),
-      initialRoute: "/home",
+      // initialRoute: "/home                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  ",
       routes: {
-        "/":(context) => loginPage(),
+        "/":(context) =>  HomePage(),
         "/home":(context) => HomePage(),
         "/login":(context) => loginPage()
       },
--- l1/flutter_catalog/lib/pages/loginPage.dart
+++ l1/flutter_catalog/lib/pages/loginPage.dart
@@ -6,15 +6,54 @@ class loginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "loginpage",style: 
-        TextStyle(
-          color:Colors.blue,
-          fontWeight: FontWeight.w900,
-          ),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+              child: Text("dss"),
+            ),
+            Text("Welcome",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                )),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 35),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "enter it",
+                      labelText: "username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "enter privately",
+                      labelText: "password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                    child: Text("sizedbox"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("my first btn");
+                    },
+                    style: TextButton.styleFrom(),
+                    child: Text("login"),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
